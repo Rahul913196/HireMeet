e25f959254a36e2d7bbc6a383ffb5183b641b9c9
Rahul Kumar <cs.rahul5599@gmail.com>
2026-02-26 13:42:47 +0530

    Revert "deployment config added "

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,30 +1,16 @@
 // const express = require("express")
 import express from "express";
-import path from "path";
 
 import {ENV} from "./lib/env.js";
 
 const app = express();
 
-const __dirname = path.resolve()
+console.log(ENV.PORT);
+console.log(ENV.DB_URL);
 
-app.get("/health", (req, res) => {
-    res.status(200).json({msg:"api is up and running"})
+app.get("/", (req, res) => {
+    res.status(200).json({msg:"success from server"})
 });
 
-app.get("/books", (req, res) => {
-    res.status(200).json({msg:"this is the books endpoint"})
-});
-
-//MAKE OUR APP  READY FOR DEPLOYMENT 
-if(ENV.NODE_ENV == "production"){
-    app.use(express.static(path.join(__dirname,"../frontend/dist")))
-
-
-app.get("/{*any}", (req,res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-});
-
-}
 app.listen(3000, () => console.log("Server is runnig on port:",ENV.PORT));
 
